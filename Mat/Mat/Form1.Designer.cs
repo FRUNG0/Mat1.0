8391a81fb0ccbcce7967356334afb5c90f1979bb
FRUNG0 <108824680+FRUNG0@users.noreply.github.com>
2023-10-03 22:37:46 +0300

    Add files via upload

---
+++ Mat/Mat/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace Mat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelderece = new System.Windows.Forms.Label();
+            this.textderece1 = new System.Windows.Forms.TextBox();
+            this.textdakika1 = new System.Windows.Forms.TextBox();
+            this.textsaniye1 = new System.Windows.Forms.TextBox();
+            this.textsaniye2 = new System.Windows.Forms.TextBox();
+            this.textdakika2 = new System.Windows.Forms.TextBox();
+            this.textderece2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buteksi = new System.Windows.Forms.Button();
+            this.butartı = new System.Windows.Forms.Button();
+            this.labeldakika = new System.Windows.Forms.Label();
+            this.labelsaniye = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelderece
+            // 
+            this.labelderece.AutoSize = true;
+            this.labelderece.Location = new System.Drawing.Point(13, 124);
+            this.labelderece.Name = "labelderece";
+            this.labelderece.Size = new System.Drawing.Size(35, 13);
+            this.labelderece.TabIndex = 0;
+            this.labelderece.Text = "label1";
+            // 
+            // textderece1
+            // 
+            this.textderece1.Location = new System.Drawing.Point(9, 32);
+            this.textderece1.Name = "textderece1";
+            this.textderece1.Size = new System.Drawing.Size(100, 20);
+            this.textderece1.TabIndex = 1;
+            // 
+            // textdakika1
+            // 
+            this.textdakika1.Location = new System.Drawing.Point(115, 32);
+            this.textdakika1.Name = "textdakika1";
+            this.textdakika1.Size = new System.Drawing.Size(100, 20);
+            this.textdakika1.TabIndex = 2;
+            // 
+            // textsaniye1
+            // 
+            this.textsaniye1.Location = new System.Drawing.Point(221, 32);
+            this.textsaniye1.Name = "textsaniye1";
+            this.textsaniye1.Size = new System.Drawing.Size(100, 20);
+            this.textsaniye1.TabIndex = 3;
+            // 
+            // textsaniye2
+            // 
+            this.textsaniye2.Location = new System.Drawing.Point(221, 85);
+            this.textsaniye2.Name = "textsaniye2";
+            this.textsaniye2.Size = new System.Drawing.Size(100, 20);
+            this.textsaniye2.TabIndex = 6;
+            // 
+            // textdakika2
+            // 
+            this.textdakika2.Location = new System.Drawing.Point(115, 85);
+            this.textdakika2.Name = "textdakika2";
+            this.textdakika2.Size = new System.Drawing.Size(100, 20);
+            this.textdakika2.TabIndex = 5;
+            // 
+            // textderece2
+            // 
+            this.textderece2.Location = new System.Drawing.Point(9, 85);
+            this.textderece2.Name = "textderece2";
+            this.textderece2.Size = new System.Drawing.Size(100, 20);
+            this.textderece2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Derece";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dakika";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Saniye";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Saniye";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(112, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Dakika";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Derece";
+            // 
+            // buteksi
+            // 
+            this.buteksi.Location = new System.Drawing.Point(165, 119);
+            this.buteksi.Name = "buteksi";
+            this.buteksi.Size = new System.Drawing.Size(75, 23);
+            this.buteksi.TabIndex = 13;
+            this.buteksi.Text = "-";
+            this.buteksi.UseVisualStyleBackColor = true;
+            this.buteksi.Click += new System.EventHandler(this.buteksi_Click);
+            // 
+            // butartı
+            // 
+            this.butartı.Location = new System.Drawing.Point(246, 119);
+            this.butartı.Name = "butartı";
+            this.butartı.Size = new System.Drawing.Size(75, 23);
+            this.butartı.TabIndex = 14;
+            this.butartı.Text = "+";
+            this.butartı.UseVisualStyleBackColor = true;
+            this.butartı.Click += new System.EventHandler(this.butartı_Click);
+            // 
+            // labeldakika
+            // 
+            this.labeldakika.AutoSize = true;
+            this.labeldakika.Location = new System.Drawing.Point(54, 124);
+            this.labeldakika.Name = "labeldakika";
+            this.labeldakika.Size = new System.Drawing.Size(35, 13);
+            this.labeldakika.TabIndex = 15;
+            this.labeldakika.Text = "label8";
+            // 
+            // labelsaniye
+            // 
+            this.labelsaniye.AutoSize = true;
+            this.labelsaniye.Location = new System.Drawing.Point(95, 124);
+            this.labelsaniye.Name = "labelsaniye";
+            this.labelsaniye.Size = new System.Drawing.Size(35, 13);
+            this.labelsaniye.TabIndex = 16;
+            this.labelsaniye.Text = "label9";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Böl";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(73, 20);
+            this.textBox1.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "label1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(98, 76);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Mat.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 68);
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Mat.Properties.Resources.download;
+            this.pictureBox2.Location = new System.Drawing.Point(82, 59);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(73, 10);
+            this.pictureBox2.TabIndex = 23;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Mat.Properties.Resources.download;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 91);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(89, 10);
+            this.pictureBox3.TabIndex = 24;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(0, 16);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Bölme";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(374, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(164, 163);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textsaniye1);
+            this.groupBox2.Controls.Add(this.labelderece);
+            this.groupBox2.Controls.Add(this.labelsaniye);
+            this.groupBox2.Controls.Add(this.textderece1);
+            this.groupBox2.Controls.Add(this.labeldakika);
+            this.groupBox2.Controls.Add(this.textdakika1);
+            this.groupBox2.Controls.Add(this.butartı);
+            this.groupBox2.Controls.Add(this.textderece2);
+            this.groupBox2.Controls.Add(this.buteksi);
+            this.groupBox2.Controls.Add(this.textdakika2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textsaniye2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(356, 163);
+            this.groupBox2.TabIndex = 27;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 188);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Matematik 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelderece;
+        private System.Windows.Forms.TextBox textderece1;
+        private System.Windows.Forms.TextBox textdakika1;
+        private System.Windows.Forms.TextBox textsaniye1;
+        private System.Windows.Forms.TextBox textsaniye2;
+        private System.Windows.Forms.TextBox textdakika2;
+        private System.Windows.Forms.TextBox textderece2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buteksi;
+        private System.Windows.Forms.Button butartı;
+        private System.Windows.Forms.Label labeldakika;
+        private System.Windows.Forms.Label labelsaniye;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Mat/Mat/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void buteksi_Click(object sender, EventArgs e)
+        {
+            int derece1=Convert.ToInt32(textderece1.Text);
+            int derece2=Convert.ToInt32(textderece2.Text);
+
+            int dakika1=Convert.ToInt32(textdakika1.Text);
+            int dakika2=Convert.ToInt32(textdakika2.Text);
+
+            int saniye1=Convert.ToInt32(textsaniye1.Text);
+            int saniye2=Convert.ToInt32(textsaniye2.Text);
+
+            int saniyeeksi=saniye1 - saniye2;
+            int dakikaeksi=dakika1 - dakika2;
+
+            if (saniyeeksi < 0)
+            {
+                saniye1 = saniye1+60;
+                dakika1 = dakika1-1;
+            }
+            if (dakikaeksi < 0)
+            {
+                dakika1 =dakika1+60;
+                derece1 = derece1 - 1;
+            }
+            
+            int saniyesonuc=saniye1 - saniye2;
+            int dakikasonuc=dakika1 - dakika2;
+            int derecesonuc=derece1 - derece2;
+
+            labelderece.Text=derecesonuc.ToString();
+            labeldakika.Text=dakikasonuc.ToString();
+            labelsaniye.Text=saniyesonuc.ToString();
+
+        }
+
+        private void butartı_Click(object sender, EventArgs e)
+        {
+            int derece1 = Convert.ToInt32(textderece1.Text);
+            int derece2 = Convert.ToInt32(textderece2.Text);
+
+            int dakika1 = Convert.ToInt32(textdakika1.Text);
+            int dakika2 = Convert.ToInt32(textdakika2.Text);
+
+            int saniye1 = Convert.ToInt32(textsaniye1.Text);
+            int saniye2 = Convert.ToInt32(textsaniye2.Text);
+
+            int saniyeartı = saniye1 + saniye2;
+            int dakikaartı = dakika1 + dakika2;
+
+            if (saniyeartı >= 60)
+            {
+                saniye1=saniye1-60;
+                dakika1=dakika1+1;
+            }
+
+            if(dakikaartı >= 60)
+            {
+                dakika1=dakika1-60;
+                derece1 = derece1+1;
+            }
+
+            int saniyesonuc = saniye1 + saniye2;
+            int dakikasonuc = dakika1 + dakika2;
+            int derecesonuc = derece1 + derece2;
+
+            labelderece.Text = derecesonuc.ToString();
+            labeldakika.Text = dakikasonuc.ToString();
+            labelsaniye.Text = saniyesonuc.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a =Convert.ToInt32(textBox1.Text);
+            int b= Convert.ToInt32(textBox2.Text);
+            label1.Text=(""+a % b);
+            label8.Text=(""+a/b);
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
